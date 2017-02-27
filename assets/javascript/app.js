0dadd990605d41add107644826f797f1287c5c23
Aaron Trierweiler <ait530@users.noreply.github.com>
2017-02-27 12:59:18 -0600

    oragnized javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,81 @@
+/*Executes code after entire page loads*/
+window.onload = function () {
+
+
+
+
+
+
+
+}
+
+
+
+var correctAnswers
+var 
+
+
+
+/*Large, clickable Start button that, when pushed, transitions to the question, answer, and timer screen
+(add hover CSS effect)*/
+function startGame() {
+
+
+};
+
+
+
+
+
+/*A timer counting down, time remaining, from say, 30 seconds. 
+
+If timer reaches 0, the game will transtion to an out of time (function?) prompt with the correct answer*/
+function timerDown () {
+
+
+};
+
+
+
+/*Stop timer will stop the timer from continusing to countdown*/
+function stopTimer () {};
+
+
+
+
+/*Transitions game to out of time prompt with the correct answer for the question. After a certain amount of time (5 secs?), transitions to the next question, or to game over screen if everything has been answered
+*/
+function outOftime () {
+
+
+};
+
+
+
+
+function question()
+
+
+
+/*Tells user "Correct!"*/
+function correctAnswer() {}
+
+
+
+/*Tell user "Nope/Incorrect!" and show the correct answer below*/
+function incorrectAnswer () {}
+
+
+
+/*Shows all done: the user's correct answers; incorrect answers; and unanswered? also shows the start over? button*/
+function endGame() {}
+
+
+
+
+/*When the start over button is clicked, the game will transtion striaght to the next question, thus resetting the game*/
+function startOver(){}
+
+
+start: function()
+
